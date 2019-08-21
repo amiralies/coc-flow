197ef1da05b6f8268f29952eb55f15be8f6f82f6
Amirali Esmaeili <esmailiamirali@gmail.com>
2019-08-22 03:05:49 +0430

    Add basic flow language client

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 // @flow
+import { LanguageClient, workspace, services, ExtensionContext } from 'coc.nvim';
+import fs from 'fs';
+import path from 'path';
+
+exports.activate = (context: ExtensionContext) => {
+  const flowConfigPath = path.join(workspace.rootPath, '.flowconfig');
+  const flowBinPath = path.join(workspace.rootPath, 'node_modules', '.bin', 'flow');
+
+  if (!fs.existsSync(flowConfigPath)) {
+    return;
+  }
+
+  const command = flowBinPath;
+  const serverOptions = { command, args: ['lsp'] };
+
+  const documentSelector = ['javascript', 'javascriptreact'].reduce(
+    (prev, language) => [...prev, { language, scheme: 'file' }, { language, scheme: 'untitled' }],
+    [],
+  );
+
+  const clientOptions = {
+    documentSelector,
+  };
+
+  const languageClient = new LanguageClient('flow', 'Flow', serverOptions, clientOptions);
+  context.subscriptions.push(services.registLanguageClient(languageClient));
+};
