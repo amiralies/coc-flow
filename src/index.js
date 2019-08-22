5e6abdb197d1545c58428ac895329e9a9b14b4b0
Amirali Esmaeili <esmailiamirali@gmail.com>
2019-08-22 17:13:53 +0430

    Add custom path flow support

--- src/index.js
+++ src/index.js
@@ -3,16 +3,29 @@ import { LanguageClient, workspace, services, ExtensionContext } from 'coc.nvim'
 import fs from 'fs';
 import path from 'path';
 
+type Config = {|
+  +enable: boolean,
+  +pathToFlow: string,
+|};
+
+const getFlowCommand = (config: Config): string => {
+  const defaultFlowBinPath = path.join(workspace.rootPath, 'node_modules', '.bin', 'flow');
+  if (fs.existsSync(defaultFlowBinPath)) {
+    return defaultFlowBinPath;
+  }
+
+  return config.pathToFlow;
+};
+
 exports.activate = (context: ExtensionContext) => {
   const flowConfigPath = path.join(workspace.rootPath, '.flowconfig');
-  const flowBinPath = path.join(workspace.rootPath, 'node_modules', '.bin', 'flow');
-  const config = workspace.getConfiguration().get('flow', {});
+  const config: Config = workspace.getConfiguration().get('flow', {});
 
   if (!fs.existsSync(flowConfigPath) || config.enable === false) {
     return;
   }
 
-  const command = flowBinPath;
+  const command = getFlowCommand(config);
   const serverOptions = { command, args: ['lsp'] };
 
   const documentSelector = ['javascript', 'javascriptreact'].reduce(
